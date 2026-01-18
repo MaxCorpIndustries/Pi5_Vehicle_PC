b3846c04edce0b11eb46135dd13480f49dbdfdbe
Maximo <38792883+MaxCorpIndustries@users.noreply.github.com>
2026-01-18 17:34:09 -0600

    testing why USB won't connect

--- Test_WebcamRecording.py
+++ Test_WebcamRecording.py
@@ -140,7 +140,8 @@ def testRTSP_Ping():
                     
             case "USB":
                 try:
-                    pingOutput = subprocess.run(["v4l2-ctl","--list-devices","|","grep","-A","1",i.ping],check=True,capture_output=True,text=True)
+                    pingOutput = subprocess.run(["v4l2-ctl","--list-devices","|","grep","-A","1",i.ping],check=True,capture_output=True,text=True,stdout=DEVNULL)
+                    print(pingOutput)
                     if(i.accessURL in pingOutput):
                         print(i.name + " Camera Online" )
                         cameras[index].readytoload = True
