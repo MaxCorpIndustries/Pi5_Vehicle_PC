0e5420143e461fa6ead91b00cd89c8b4ecb7e70c
Maximo <38792883+MaxCorpIndustries@users.noreply.github.com>
2026-01-25 13:19:00 -0600

    added basic kivy using kivy.my file

--- Central_System.py
+++ Central_System.py
@@ -1,8 +1,14 @@
 import CORE_WebcamRecording as cam
+from kivy.app import App
+
+
+class TestApp(App):
+    pass
 
 def main():
     print('started')
     cam.StartVideoProcess()
+    TestApp().run()
 
 # Run the main() function
 if __name__ == '__main__':
