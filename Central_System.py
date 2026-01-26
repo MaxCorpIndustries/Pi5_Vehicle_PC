7f5df6db76d0a9a6f4d5ca83d8463332a2e5c808
Maximo <38792883+MaxCorpIndustries@users.noreply.github.com>
2026-01-25 22:57:20 -0600

    bugh

--- Central_System.py
+++ Central_System.py
@@ -96,7 +96,7 @@ class MainApp(App):
         print('starting cams')
         #CoreCams.initializeInternalNetwork()
         
-        self.capture = cv2.VideoCapture("/dev/video0")
+        self.capture = cv2.VideoCapture("/dev/video0",cv2.CAP_V4L2)
                 # Request resolution
         #self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         #self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
