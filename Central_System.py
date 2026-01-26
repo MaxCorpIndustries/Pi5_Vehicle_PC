670b41c87dc2319d7ddf2da4d7c32ac0d59ce0a8
Maximo <38792883+MaxCorpIndustries@users.noreply.github.com>
2026-01-25 18:37:37 -0600

    added SlideTransition linux fix

--- Central_System.py
+++ Central_System.py
@@ -8,7 +8,7 @@ import signal
 from kivy.config import Config
 Config.set('graphics', 'width', '1024')
 Config.set('graphics', 'height', '600')
-Config.set('graphics', 'fullscreen', 'auto')
+#Config.set('graphics', 'fullscreen', 'auto')
 
 
 from kivy.clock import Clock
