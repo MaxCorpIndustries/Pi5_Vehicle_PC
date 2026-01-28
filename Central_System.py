15757798cf5895a899ec2cef6ffeb3af408391c3
Maximo <38792883+MaxCorpIndustries@users.noreply.github.com>
2026-01-28 13:07:14 -0600

    added widget module

--- Central_System.py
+++ Central_System.py
@@ -29,6 +29,7 @@ from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, SlideTransition
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 Window.borderless = True
 
